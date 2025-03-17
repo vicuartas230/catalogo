1e553731833983cf9591db55592bf365e19f89a1
vicuartas230 <vicuartas230@gmail.com>
2025-03-17 18:32:13 -0500

    Subir entidad fabrica

---
+++ src/main/java/com/egg/catalogo/entidades/Fabrica.java
@@ -0,0 +1,36 @@
+package com.egg.catalogo.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fabrica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    public Fabrica() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
